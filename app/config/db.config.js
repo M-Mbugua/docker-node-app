436a314dd094c15fda5dc39e2766860daf128097
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-16 21:42:18 +0000

    Containerized the application

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,11 @@
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
 module.exports = {
-    url: "mongodb://0.0.0.0:27017/employee_db"
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
   };
